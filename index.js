b54429a6e30403c387d975784a8146fe11af8f2e
scholastica ngene <nnemms@gmail.com>
2022-09-22 00:40:25 +0100

    added more files

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
 
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
 ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
